e6e0e5f132f781d2e5c9233c15a39dd9c00dfadc
Omer <omerg863@gmail.com>
2022-05-04 13:46:49 +0300

    fix first day noon missing name

--- mishmar/views.py
+++ mishmar/views.py
@@ -2286,18 +2286,10 @@ def uplaod_organize(request, organization):
                 # noon
                 index = 0
                 for key in noon_keys:
-                    print("1")
-                    print(weeks_dicts[num_week])
-                    print(names_days[f'day{names_x}_noon'])
                     if index < len(noon_keys) - 1:
-                        print("entrence")
                         insert_random(weeks_dicts[num_week], names_days[f'day{names_x}_noon'], f'{key.id}', x, 0)
                     else:
-                        print("all")
                         insert_all_to_form(weeks_dicts[num_week], names_days[f'day{names_x}_noon'], x, f'{key.id}')
-                    print("2")
-                    print(weeks_dicts[num_week])
-                    print(names_days[f'day{names_x}_noon'])
                     index += 1
             # morning opening
             else:
